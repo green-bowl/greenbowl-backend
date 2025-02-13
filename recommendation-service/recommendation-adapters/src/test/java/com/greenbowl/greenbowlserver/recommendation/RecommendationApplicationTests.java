2044276a28ed6a8f2e54780f07c16e387919fb0e
hellmir <hellmir610@gmail.com>
2025-02-13 21:01:58 +0900

    [TEAM15-23] chore(recommendation): Recommendation Service 모듈 추가 (#4)

---
+++ recommendation-service/recommendation-adapters/src/test/java/com/greenbowl/greenbowlserver/recommendation/RecommendationApplicationTests.java
@@ -0,0 +1,11 @@
+package com.greenbowl.greenbowlserver.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
