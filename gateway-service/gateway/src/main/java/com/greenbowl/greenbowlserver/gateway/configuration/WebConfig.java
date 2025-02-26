9fe8fbe6fc18d8e5d424eb267735334b8dba7b49
adlaylatte <dgs2288@naver.com>
2025-02-26 22:15:41 +0900

    [TEAM15-395] feat(gateway): gateway 모듈 구현 (#42)
    - gateway-service 모듈 추가
    - application.yml gateway 정보 추가
    - settings.gradle 모듈 정보 추가
    - cors config 설정 추가

---
+++ gateway-service/gateway/src/main/java/com/greenbowl/greenbowlserver/gateway/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.greenbowl.greenbowlserver.gateway.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
