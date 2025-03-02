2cb7fd8d89dc75c193d67d65dac1e70c6abcbdd2
hellmir <hellmir610@gmail.com>
2025-03-02 21:34:10 +0900

    [TEAM15-549] feat(user): 회원 서비스의 로그인 응답 시 엑세스 토큰을 헤더에 보내도록 변경 (#96)
    - 엑세스 토큰을 Authorization 헤더에 삽입해서 응답하도록 변경

--- recipe-service/recipe-adapters/src/main/java/com/greenbowl/greenbowlserver/recipe/configuration/SwaggerConfig.java
+++ recipe-service/recipe-adapters/src/main/java/com/greenbowl/greenbowlserver/recipe/configuration/SwaggerConfig.java
@@ -43,8 +43,8 @@ SwaggerConfig {
                 .build()
                 .apiInfo(apiInfo())
                 .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()));
-//                .pathMapping("/api/recipe");
+                .securitySchemes(Arrays.asList(apiKey()))
+                .pathMapping("/api/recipes");
     }
 
     private SecurityContext securityContext() {
