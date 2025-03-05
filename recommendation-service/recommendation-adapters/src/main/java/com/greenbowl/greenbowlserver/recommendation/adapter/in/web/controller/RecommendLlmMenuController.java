2a34dde52ffa6abedcdc212331abb1f67a00c83b
hellmir <hellmir610@gmail.com>
2025-03-05 12:42:20 +0900

    [TEAM15-586] docs(recommendation): 클라이언트는 Swagger 문서를 통해 메뉴 추천 서비스 이용 가이드를 확인할 수 있다 (#109)
    - 메뉴 추천 서비스 엔드포인트에 Swagger 문서 추가

--- recommendation-service/recommendation-adapters/src/main/java/com/greenbowl/greenbowlserver/recommendation/adapter/in/web/controller/RecommendLlmMenuController.java
+++ recommendation-service/recommendation-adapters/src/main/java/com/greenbowl/greenbowlserver/recommendation/adapter/in/web/controller/RecommendLlmMenuController.java
@@ -4,6 +4,8 @@ import com.greenbowl.greenbowlserver.common.adapter.in.WebAdapter;
 import com.greenbowl.greenbowlserver.recommendation.adapter.in.web.mapper.RecommendationRequestToCommandMapper;
 import com.greenbowl.greenbowlserver.recommendation.adapter.in.web.request.MenuOptionsRequest;
 import com.greenbowl.greenbowlserver.recommendation.port.in.usecase.RecommendLlmMenuUseCase;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +24,26 @@ import static org.springframework.http.HttpStatus.OK;
 public class RecommendLlmMenuController {
     private final RecommendLlmMenuUseCase recommendLlmMenuUseCase;
 
+    private static final String GET_MENU_LLM_JSON = "LLM을 통해 메뉴 정보 JSON 데이터를 추천 받는 엔드포인트";
+    private static final String GET_MENU_LLM_JSON_DESCRIPTION = "LLM을 통해 메뉴 정보 데이터를 JSON 형식으로 추천 받을 수 있습니다.";
+
+    private static final String AVAILABLE_INGREDIENTS_VALUE = "사용 가능한 재료명 목록";
+    private static final String AVAILABLE_INGREDIENTS_EXAMPLE = "돼지고기, 감자, 양파";
+
+    private static final String COOKING_TIME_LIMIT_VALUE = "조리 제한 시간";
+    private static final String COOKING_TIME_LIMIT_EXAMPLE = "30분";
+
+    private static final String CUISINE_TYPE_VALUE = "요리 유형";
+    private static final String CUISINE_TYPE_EXAMPLE = "한식";
+
+    @ApiOperation(value = GET_MENU_LLM_JSON, notes = GET_MENU_LLM_JSON_DESCRIPTION)
     @GetMapping()
     public ResponseEntity<String> requestMenuLLM(
+            @ApiParam(value = AVAILABLE_INGREDIENTS_VALUE, defaultValue = AVAILABLE_INGREDIENTS_EXAMPLE)
             @RequestParam List<String> ingredients,
+            @ApiParam(value = COOKING_TIME_LIMIT_VALUE, defaultValue = COOKING_TIME_LIMIT_EXAMPLE)
             @RequestParam List<String> cookingTimeLimit,
+            @ApiParam(value = CUISINE_TYPE_VALUE, defaultValue = CUISINE_TYPE_EXAMPLE)
             @RequestParam List<String> cuisineType
     ) {
         String jsonResponse = recommendLlmMenuUseCase.receiveLLMRecommendedMenu(
